79f2a4ad7adb0962a9206eb0dc7803cb81038d13
phyxius23 <30ntin@gmail.com>
2023-07-04 13:03:40 +0200

    aggiunto test createDogSitter

--- src/test/java/org/antoniotrentin/epidogsitting/EpidogsittingApplicationTests.java
+++ src/test/java/org/antoniotrentin/epidogsitting/EpidogsittingApplicationTests.java
@@ -8,94 +8,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.antoniotrentin.epidogsitting.entities.DogSitter;
+import org.antoniotrentin.epidogsitting.entities.payloads.DogSitterCreatePayload;
 import org.antoniotrentin.epidogsitting.repositories.DogSitterRepository;
+import org.antoniotrentin.epidogsitting.repositories.UserRepository;
 import org.antoniotrentin.epidogsitting.services.DogSitterService;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
-//@AutoConfigureMockMvc
+@AutoConfigureMockMvc
 class EpidogsittingApplicationTests {
 
-	//	@Mock
-	//	DogSitterRepository dogSitterRepo;
+	private String serviceUrl = "http://localhost:3001/";
 
-	//	@MockBean
-	//	DogSitterService dogSitterService;
-	//
-	//	@Autowired
-	//	private MockMvc mockMvc;
-	//
-	//	ObjectMapper mapper = new ObjectMapper();
-	//
-	//	@Test
-	//	public void when_save_dogsitter_it_should_return_dogsitter() throws Exception {
-	//		DogSitterCreatePayload request = new DogSitterCreatePayload();
-	//		request.setName("Mario");
-	//		//		request.setSurname("Rossi");
-	//		//		request.setEmail("mariorossi@gmail.com");
-	//		//		request.setPassword("1234");
-	//
-	//		DogSitter newDogSitter = new DogSitter();
-	//		newDogSitter.setName(request.getName());
-	//
-	//		when(dogSitterService.create(any(DogSitterCreatePayload.class))).thenReturn(newDogSitter);
-	//	}
+	@Autowired
+	private MockMvc mockMvc;
 
-	//	private String serviceUrl = "http://localhost:5432/";
-
-	//	@Test
-	//	void shouldGetDogSittersList() throws Exception {
-	//		mockMvc.perform(MockMvcRequestBuilders.get("/dogsitters")).andExpect(status().is5xxServerError());
-	//	}
-
-	//@Autowired
-	//	@Mock
-	//	DogSitterRepository dogSitterRepo;
-
-	//@Autowired
-	//	@InjectMocks
-	//	DogSitterService dogSitterService;
-
-	//	@Test
-	//	public void testGetDogSittersOK() throws Exception {
-	//		String servicePath = "/dogsitters";
-	//		int page = 0;
-	//		int size = 10;
-	//		String sortBy = "id";
-	//		mockMvc
-	//				.perform(get(serviceUrl + servicePath).param("page", String.valueOf(page)).param("size", String.valueOf(size))
-	//						.param("sortBy", sortBy))
-	//				.andDo(print()).andExpect(status().isOk()).andExpect(content().contentType(MediaType.APPLICATION_JSON));
-	//	}
-
-	//	@Test
-	//	public void testFindAllDogSitters() {
-	//
-	//		// Mocking the repository
-	//		Pageable pageable = PageRequest.of(0, 10, Sort.by("sortBy"));
-	//		List<DogSitter> dogSitters = new ArrayList<>();
-	//		dogSitters.add(new DogSitter());
-	//		Page<DogSitter> page = new PageImpl(dogSitters, pageable, 1);
-	//
-	//		when(dogSitterRepo.findAll(pageable)).thenReturn(page);
-	//
-	//		// Calling the service method
-	//		Page<DogSitter> result = dogSitterService.readAll(0, 10, "sortBy", "", "");
-	//
-	//		// Assertions
-	//				assertNotNull(result);
-	//				assertEquals(1, result.getTotalElements());
-	//		// Additional assertions based on your expected data
-	//
-	//	}
+	@Mock
+	private UserRepository userRepo;
 
 	@Mock
 	private DogSitterRepository dogSitterRepo;
@@ -105,7 +46,6 @@ class EpidogsittingApplicationTests {
 
 	@Test
 	public void testReadAllDogSitters() {
-
 		// Mocking the repository
 		Pageable pageable = PageRequest.of(0, 10, Sort.by("sortBy"));
 		List<DogSitter> dogSitters = new ArrayList<>();
@@ -121,8 +61,38 @@ class EpidogsittingApplicationTests {
 		assertNotNull(result);
 		assertEquals(1, result.getTotalElements());
 		// Additional assertions based on your expected data
-
 	}
+
+	@Test
+	public void testCreateDogSitter() {
+		// Mocking the repository
+		DogSitterCreatePayload payload = new DogSitterCreatePayload();
+		payload.setName("Test");
+		payload.setSurname("User");
+		payload.setEmail("testuser@example.com");
+		payload.setPassword("password");
+		when(dogSitterRepo.save(Mockito.any(DogSitter.class))).thenReturn(new DogSitter());
+
+		// Calling the service method
+		DogSitter result = dogSitterService.create(payload);
+
+		// Assertions
+		assertNotNull(result);
+		// Additional assertions based on your expected data
+	}
+
+	// Manca l'autorizzazione nell'header per poter eseguire il test correttamente
+	//@Test
+	//	public void testGetDogSitters() throws Exception {
+	//		String servicePath = "/dogsitters";
+	//		int page = 0;
+	//		int size = 10;
+	//		String sortBy = "id";
+	//		mockMvc
+	//				.perform(get(serviceUrl + servicePath).param("page", String.valueOf(page)).param("size", String.valueOf(size))
+	//						.param("sortBy", sortBy))
+	//				.andDo(print()).andExpect(status().isOk()).andExpect(content().contentType(MediaType.APPLICATION_JSON));
+	//	}
 
 	@Test
 	void contextLoads() {
