b52426f775fb13fa276b41e9e848321a7267756a
phyxius23 <30ntin@gmail.com>
2023-07-04 12:05:41 +0200

    aggiunto test readAllDogSitters

--- src/main/java/org/antoniotrentin/epidogsitting/entities/payloads/DogSitterCreatePayload.java
+++ src/main/java/org/antoniotrentin/epidogsitting/entities/payloads/DogSitterCreatePayload.java
@@ -4,10 +4,12 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class DogSitterCreatePayload {
 
 	@NotNull(message = "Il nome è obbligatorio")
--- src/test/java/org/antoniotrentin/epidogsitting/EpidogsittingApplicationTests.java
+++ src/test/java/org/antoniotrentin/epidogsitting/EpidogsittingApplicationTests.java
@@ -1,27 +1,60 @@
 package org.antoniotrentin.epidogsitting;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.antoniotrentin.epidogsitting.entities.DogSitter;
+import org.antoniotrentin.epidogsitting.repositories.DogSitterRepository;
+import org.antoniotrentin.epidogsitting.services.DogSitterService;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 @SpringBootTest
-@AutoConfigureMockMvc
+//@AutoConfigureMockMvc
 class EpidogsittingApplicationTests {
 
-	@Autowired
-	private MockMvc mockMvc;
+	//	@Mock
+	//	DogSitterRepository dogSitterRepo;
 
-	private String serviceUrl = "http://localhost:5432/";
+	//	@MockBean
+	//	DogSitterService dogSitterService;
+	//
+	//	@Autowired
+	//	private MockMvc mockMvc;
+	//
+	//	ObjectMapper mapper = new ObjectMapper();
+	//
+	//	@Test
+	//	public void when_save_dogsitter_it_should_return_dogsitter() throws Exception {
+	//		DogSitterCreatePayload request = new DogSitterCreatePayload();
+	//		request.setName("Mario");
+	//		//		request.setSurname("Rossi");
+	//		//		request.setEmail("mariorossi@gmail.com");
+	//		//		request.setPassword("1234");
+	//
+	//		DogSitter newDogSitter = new DogSitter();
+	//		newDogSitter.setName(request.getName());
+	//
+	//		when(dogSitterService.create(any(DogSitterCreatePayload.class))).thenReturn(newDogSitter);
+	//	}
 
-	@Test
-	void shouldGetDogSittersList() throws Exception {
-		mockMvc.perform(MockMvcRequestBuilders.get("/dogsitters")).andExpect(status().is5xxServerError());
-	}
+	//	private String serviceUrl = "http://localhost:5432/";
+
+	//	@Test
+	//	void shouldGetDogSittersList() throws Exception {
+	//		mockMvc.perform(MockMvcRequestBuilders.get("/dogsitters")).andExpect(status().is5xxServerError());
+	//	}
 
 	//@Autowired
 	//	@Mock
@@ -30,10 +63,6 @@ class EpidogsittingApplicationTests {
 	//@Autowired
 	//	@InjectMocks
 	//	DogSitterService dogSitterService;
-
-	@Test
-	void contextLoads() {
-	}
 
 	//	@Test
 	//	public void testGetDogSittersOK() throws Exception {
@@ -67,4 +96,36 @@ class EpidogsittingApplicationTests {
 	//		// Additional assertions based on your expected data
 	//
 	//	}
+
+	@Mock
+	private DogSitterRepository dogSitterRepo;
+
+	@InjectMocks
+	private DogSitterService dogSitterService;
+
+	@Test
+	public void testReadAllDogSitters() {
+
+		// Mocking the repository
+		Pageable pageable = PageRequest.of(0, 10, Sort.by("sortBy"));
+		List<DogSitter> dogSitters = new ArrayList<>();
+		dogSitters.add(new DogSitter("Aldo", "Baglio", "aldobaglio@gmail.com", "1234"));
+		Page<DogSitter> page = new PageImpl<>(dogSitters, pageable, 1);
+
+		when(dogSitterRepo.findAll(pageable)).thenReturn(page);
+
+		// Calling the service method
+		Page<DogSitter> result = dogSitterService.readAll(0, 10, "sortBy", "", "", null);
+
+		// Assertions
+		assertNotNull(result);
+		assertEquals(1, result.getTotalElements());
+		// Additional assertions based on your expected data
+
+	}
+
+	@Test
+	void contextLoads() {
+	}
+
 }
